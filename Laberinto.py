c357c1d6be4c4e8a813ac3b606922fac4be3f1c7
José A. Gallud <jgallud@gmail.com>
2023-02-15 11:50:49 +0100

    primera version

---
+++ Laberinto.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from Habitacion import Habitacion
+
+class Laberinto(Habitacion):
+    def __init__(self):
+        self.habitaciones = None
+        self.habitaciones = []
+
